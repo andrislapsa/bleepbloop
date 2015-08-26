591b994e90ba94dadb2e9b8c11c8c17d6e5549cf
Andris Lapsa <lapsaaa@gmail.com>
2015-08-26 22:03:35 +0300

    style

--- main.js
+++ main.js
@@ -1,57 +1,57 @@
 var BleepBloop = {
-	/**
-	 * bleep - 1
-	 * bloop - 0
-	 */
-	playSound: function (bit) {
-		var file = parseInt(bit) ? "bleep" : "bloop";
-			audio = new Audio(file + ".mp3");
+    /**
+     * bleep - 1
+     * bloop - 0
+     */
+    playSound: function (bit) {
+        var file = parseInt(bit) ? "bleep" : "bloop",
+            audio = new Audio(file + ".mp3");
 
-		audio.play();
-	},
+        audio.play();
+    },
 
-	validate: function (bits) {
-		var key, bit, valid, result = [];
+    validate: function (bits) {
+        var key, bit, valid, result = [];
 
-		for (key in bits) {
-			bit = parseInt(bits[key], 10);
-			valid = bit === 1 || bit === 0;
+        for (key in bits) {
+            bit = parseInt(bits[key], 10);
+            valid = bit === 1 || bit === 0;
 
-			if (valid) {
-				result.push(bit);
-			}
-		}
+            if (valid) {
+                result.push(bit);
+            }
+        }
 
-		return result;
-	},
+        return result;
+    },
 
-	processQueue: function (bits) {
-		var bit = bits.shift(),
-			self = this;
+    processQueue: function (bits) {
+        var bit = bits.shift(),
+            self = this;
 
-		this.playSound(bit);
-		
-		if (!bits.length) {
-			return;
-		}
+        this.playSound(bit);
+        
+        if (!bits.length) {
+            return;
+        }
 
-		setTimeout(function () {
-			self.processQueue(bits);
-		}, 400);
-	},
+        setTimeout(function () {
+            self.processQueue(bits);
+        }, 400);
+    },
 
-	translate: function (input) {
-		var bits = this.validate(input.split(""));
+    translate: function (input) {
+        var bits = this.validate(input.split(""));
 
-		this.processQueue(bits);
-	}
+        this.processQueue(bits);
+    }
 };
 
 window.addEventListener("load", function () {
-	var input = document.getElementById("input"),
-		button = document.getElementById("do-it");
+    var input = document.getElementById("input"),
+        button = document.getElementById("do-it");
 
-	button.addEventListener("click", function () {
-		BleepBloop.translate(input.value);
-	});
+    button.addEventListener("click", function () {
+        BleepBloop.translate(input.value);
+    });
 });
